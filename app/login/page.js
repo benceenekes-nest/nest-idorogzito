13c7d66be26ff09a11b0b9ef47b0d9a67f1fde57
benceenekes-nest <bence.enekes@nestcom.hu>
2026-07-08 21:01:40 +0200

    Add files via upload

--- app/login/page.js
+++ app/login/page.js
@@ -3,7 +3,8 @@ import { signIn } from "next-auth/react";
 export default function Login(){
   return (
     <div className="wrap"><div className="center">
-      <h1>NEST napi időrögzítő</h1>
+      <img src="/nest-logo.svg" alt="NEST" />
+      <h1>Napi időrögzítő</h1>
       <p className="muted">Lépj be a céges Google-fiókoddal (@nestcom.hu).</p>
       <button className="btn" onClick={()=>signIn("google",{ callbackUrl:"/" })}>Belépés Google-fiókkal</button>
     </div></div>
